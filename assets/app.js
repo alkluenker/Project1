8ad8b5e347a3d33187646ec8f1d53ba7d7430b9b
Angela Kluenker <alkluenker@hotmail.com>
2020-02-03 18:37:21 -0600

    api working

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,122 @@
-var queryURL = "https://core.ac.uk:443/api-v2/search/evolution?page=1&pageSize=10&apiKey=cWTu8XQENxOvfGUP5LCbh6IjiRzmoYFq";
-    
-    //"https://wger.de/api/v2/exerciseinfo/";
+var search = $("#search-input").val().trim();
 
+var results = "";
+
+var resultsTitle = "";
+
+var fullText = "";
+
+$("#search").on("click", function(event) {
+
+    event.preventDefault();
+
+    search = $("#search-input").val().trim();
+
+    
+
+    //var searchInquiry = $(this).attr("data-name");
+
+    //console.log(searchInquiry);
+
+})
+    
+$("#search").on("click", function(event) {
+
+    event.preventDefault;
+
+   
+
+    var queryURL = "https://core.ac.uk:443/api-v2/articles/search/" + search + "?page=1&pageSize=10&metadata=true&fulltext=true&citations=false&similar=false&duplicate=false&urls=true&faithfulMetadata=false&apiKey=cWTu8XQENxOvfGUP5LCbh6IjiRzmoYFq"
+    
+        
+    
     $.ajax({
-      url: queryURL,
-      method: "GET"
+        url: queryURL,
+        method: "GET"
     }).then(function(response) {
-      console.log(response.data);
+
+        
+
+        $("#viewresults").empty();
+
+        for (i = 0; i < response.data.length; i++) {
+
+            results = response.data;
+
+            resultsTitle = results[i].title;
+
+            fullText = results[i].fulltextIdentifier;
+
+            console.log(fullText);
+
+            
+
+            
+
+            //resultsTitle = $("<a>");
+
+           // resultsTitle = $("<button>");
+
+            
+
+            //resultsTitle.attr("data-full", fullTextUrl);
+
+            //var fullTextUrl = $("<a>");
+
+            //fullTextUrl.attr("href", results.fulltextUrls[0]);
+
+            //var fullTextUrl = results.fulltextUrls[0];
+
+            //var fullText = $("href").attr("src", fullTextUrl);
+
+            
+            
+
+            //fullText.addClass("fullUrl");
+            
+
+            
+
+            //var url = $("<a>").attr("href", fullText);
+
+            
+
+            $("#viewresults").append(resultsTitle + "<br>");
+
+            $("#viewresults").append(fullText + "<br>");
+
+            //$("#viewresults").append(results.fullText);
+
+            console.log(resultsTitle);
+
+            //console.log(response.data[i]._source.downloadUrl);
+
+            //resultsTitle.addClass("results");
+
+           
+
+        }
+
+
+        
+
+        $("#viewresults").on("click", function() {
+            
+
+            console.log(this);
+
+            var resultUrl = $(this).fullText
+
+            
+            console.log("clicked");
+
+        })
+
+        
+
     });
+})
+
+
+
+
